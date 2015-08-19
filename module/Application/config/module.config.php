2a6e87fffa68f3b68b16fd78bb206059d1d7e0ed
Breno Grillo <bsampaio@outlook.com>
2015-08-19 18:31:41 -0300

    Adicionando o link de adição à navbar-top

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -20,6 +20,16 @@ return array(
                     ),
                 ),
             ),
+            'addAlbum' => array(
+                'type' => 'Zend\Mvc\Router\Http\Literal',
+                'options' => array(
+                    'route'    => '/album/add',
+                    'defaults' => array(
+                        'controller' => 'Album\Controller\Album',
+                        'action'     => 'add',
+                    ),
+                ),
+            ),
             // The following is a route to simplify getting started creating
             // new controllers and actions without needing to create a new
             // module. Simply drop new controllers in, and you can access them
