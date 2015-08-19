3ad7500c76b48530ac3398415a1143233f47b543
Breno Grillo <bsampaio@outlook.com>
2015-08-19 16:14:44 -0300

    Terminando a ação de Index

--- module/Album/config/module.config.php
+++ module/Album/config/module.config.php
@@ -13,32 +13,34 @@
  */
 
 return [
-  'controllers' => [
-      'invokables' => [
-          'Album\Controller\Album' => 'Album\Controller\AlbumController',
-      ],
-      'view_manager' => [
-          'template_path_stack' => [
-              'album' => __DIR__ . '/../view',
-          ],
-      ],
-      'router' => [
-          'routes' => [
-              'album' => [
-                  'type' => 'segment',
-                  'options' => [
-                      'route' => '/album[/:action][/:id]',
-                      'contraints' => [
-                          'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
-                          'id' => '[0-9]+',
-                      ],
-                      'defaults' => [
-                          'controller' => 'Album\Controller\Album',
-                          'action' => 'index',
-                      ],
-                  ],
-              ],
-          ],
-      ],
-  ]  
+    'controllers' => [
+        'invokables' => [
+            'Album\Controller\Album' => 'Album\Controller\AlbumController',
+        ],
+    ],
+    
+   'view_manager' => [
+        'template_path_stack' => [
+            'album' => __DIR__ . '/../view',
+        ],
+    ],
+    
+    'router' => [
+        'routes' => [
+            'album' => [
+                'type' => 'segment',
+                'options' => [
+                    'route' => '/album[/:action][/:id]',
+                    'contraints' => [
+                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                        'id' => '[0-9]+',
+                    ],
+                    'defaults' => [
+                        'controller' => 'Album\Controller\Album',
+                        'action' => 'index',
+                    ],
+                ],
+            ],
+        ],
+    ],
 ];
--- module/Album/src/Album/Controller/AlbumController.php
+++ module/Album/src/Album/Controller/AlbumController.php
@@ -11,14 +11,18 @@
  *
  * @author Breno Grillo
  */
-
+namespace Album\Controller;
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
 
 class AlbumController extends AbstractActionController {
     
+    protected $albumTable;
+    
     public function indexAction() {
-        parent::indexAction();
+        return new ViewModel([
+            'albums' => $this->getAlbumTable()->fetchAll(),
+        ]);
     }
     
     public function addAction(){
@@ -31,5 +35,14 @@ class AlbumController extends AbstractActionController {
     
     public function deleteAction(){
         
+    }
+    
+    public function getAlbumTable() {
+        if(! $this->albumTable) {
+            $sm = $this->getServiceLocator();
+            $this->albumTable = $sm->get('Album\Model\AlbumTable');
+        }
+        
+        return $this->albumTable;
     }
 }
